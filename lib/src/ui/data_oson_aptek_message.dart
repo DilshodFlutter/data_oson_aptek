00245818f8c943843818724d66c68a2922cf8a55
dilshod_egamberdiyev <www.emacho900@gmail.com>
2022-08-01 16:56:29 +0500

    uitrd

--- lib/src/ui/data_oson_aptek_message.dart
+++ lib/src/ui/data_oson_aptek_message.dart
@@ -1,3 +1,5 @@
+import 'package:data_oson_aptek/src/block/data_oson_messag_block.dart';
+import 'package:data_oson_aptek/src/model/oson_aptek_messag_model.dart';
 import 'package:flutter/material.dart';
 
 class DataOsonAptekMessage extends StatefulWidget {
@@ -9,7 +11,51 @@ class DataOsonAptekMessage extends StatefulWidget {
 
 class _DataOsonAptekMessageState extends State<DataOsonAptekMessage> {
   @override
+  void initState() {
+    super.initState();
+    dataOsonAptekBlock.allOsonAptek();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("qwer"),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: 300,
+            width: 70,
+            color: Colors.red,
+          ),
+          StreamBuilder<List<OsonAptekMessageModel>>(
+              stream: dataOsonAptekBlock.getOsonAptek,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  List<OsonAptekMessageModel> data = snapshot.data!;
+                  return ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            Text(
+                              data[index].name,
+                              style: const TextStyle(
+                                fontSize: 30,
+                                color: Colors.black45,
+                              ),
+                            ),
+                            const SizedBox(height: 20.0),
+                            Text(data[index].id.toString()),
+                          ],
+                        );
+                      });
+                }
+                return Container();
+              })
+        ],
+      ),
+    );
   }
 }
