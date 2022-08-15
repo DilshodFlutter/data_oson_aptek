062dade92f4997c8b96ddaa584d075a7e1f3baa2
dilshod_egamberdiyev <www.emacho900@gmail.com>
2022-08-15 12:33:48 +0500

    qwas

---
+++ lib/src/database/database_helper.dart
@@ -0,0 +1,38 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper.internal();
+
+  factory DatabaseHelper() => _instance;
+
+  final String tableName = 'qwertyTable';
+  final String columnId = 'id';
+  final String columnName = 'name';
+
+  static Database? _db;
+
+  DatabaseHelper.internal();
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db!;
+    }
+    _db = await initDb();
+    return _db!;
+  }
+
+  initDb() async {
+    String databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'ajssa.db');
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return db;
+  }
+
+  void _onCreate(Database db, int newVersion) async {
+    await db.execute('CREATE TABLE $tableName('
+        '$columnName TEXT,'
+        '$columnId INTEGER PRIMARY KEY');
+  }
+
+}
