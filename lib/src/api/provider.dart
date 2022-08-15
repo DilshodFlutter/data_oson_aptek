67b4fca2c3dc2e773c39216892f8053ff1212423
dilshod_egamberdiyev <www.emacho900@gmail.com>
2022-08-15 16:21:43 +0500

    poknhytfcsq

--- lib/src/api/provider.dart
+++ lib/src/api/provider.dart
@@ -1,6 +1,16 @@
+import 'dart:convert';
+
+import 'package:data_oson_aptek/src/model/oson_aptek_messag_model.dart';
+import 'package:http/http.dart' as http;
+
 class AppProvider {
   static String baseUrl = "https://test.osonapteka.uz/api/m11/errormessage";
 
-
-
+  Future<List<OsonAptekMessageModel>> getOsonAptek() async {
+    String url = baseUrl;
+    http.Response response = await http.get(Uri.parse(url));
+    final osonAptekMessageModel =
+        osonAptekaMessageModelFromJson(utf8.decode(response.bodyBytes));
+    return osonAptekMessageModel;
+  }
 }
